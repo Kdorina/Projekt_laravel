f39e695ee6788fdaaa557966ddce578f02a6c57c
Kovács Dorina <kovacsdorina2@gmail.com>
2022-12-15 11:56:50 +0100

    védett store,update,destroy

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware'=>['auth:sanctum']]);
+Route::group(['middleware'=>['auth:sanctum'], function(){
+    Route::post('/newProduct', [ProductController::class, "store" ]); 
+    Route::put('/update/{id}', [ProductController::class, "update" ]); 
+    Route::delete('/delete/{id}', [ProductController::class, "destroy" ]); 
+}]);
 
 Route::post('/register', [AuthController::class, "signUp" ]); 
 Route::post('/login', [AuthController::class, "signIn" ]); 
 Route::get('/prod', [ProductController::class, "index" ]); 
-Route::post('/newProduct', [ProductController::class, "store" ]); 
 Route::get('/show/{id}', [ProductController::class, "show" ]); 
-Route::put('/update/{id}', [ProductController::class, "update" ]); 
-Route::delete('/delete/{id}', [ProductController::class, "destroy" ]); 
+
