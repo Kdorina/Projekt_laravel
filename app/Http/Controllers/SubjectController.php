9276287389e4585600e2e9f768856501d2fc3569
Kovács Dorina <kovacsdorina2@gmail.com>
2023-01-10 17:58:23 +0100

    subjectController működik

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -11,62 +11,104 @@ use App\Http\Resources\Subject as SubjectResource;
 class SubjectController extends BaseController
 {
         public function index(){
-            $subject = Subject::all();
-            return $this->sendResponse( SubjectResource::collection($subject), "OK");
+            return Subject::all();
+            // $subject = Subject::all();
+            // return $this->sendResponse( SubjectResource::collection($subject), "OK");
         }
     
     
         public function store(Request $request){
     
-            $input = $request->all();
-            $validator = Validator::make($input, [
+            return Subject::create($request->all());
+        //     $input = $request->all();
+        //     $validator = Validator::make($input, [
     
-                "subject" => "required",
+        //         "subject" => "required",
                
-            ]);
+        //     ]);
     
-            if($validator->fails()){
-                 return $this->sendError($validator->errors());
+        //     if($validator->fails()){
+        //          return $this->sendError($validator->errors());
             
-             }
+        //      }
     
-            $subject = Subject::create( $input );
-            // print_r("siker");
-            return $this->sendResponse(new SubjectResource( $subject), "Post létrehozva");
+        //     $subject = Subject::create( $input );
+        //     // print_r("siker");
+        //     return $this->sendResponse(new SubjectResource( $subject), "Tantárgy létrehozva");
+        
         }
         public function show( $id){
-            $subject = Subject::find($id);
+            // $subject = Subject::find($id);
     
-            if( is_null($subject)){
-                return $this->sendError("Post nem létezik");
-            }
-            return $this->sendResponse( new SubjectResource ($subject), "Post betöltve" );
+            // if( is_null($subject)){
+            //     return $this->sendError("Tantárgy nem létezik");
+            // }
+            // return $this->sendResponse( new SubjectResource ($subject), "Tantárgy betöltve" );
+
+            return Subject::find($id);
         }
     
         public function update(Request $request, $id){
-            $input = $request->all();
+            // $input = $request->all();
     
-            $validator = Validator::make($input, [
+            // $validator = Validator::make($input, [
     
-                "subject" => "required",
+            //     "subject" => "required",
                 
     
-            ]);
+            // ]);
     
-            if($validator->fails()){
-                return $this-sendError($validator->errors());
+            // if($validator->fails()){
+            //     return $this-sendError($validator->errors());
+            // }
+    
+            // $subject = Subject::find( $id );
+            // $subject->update( $request->all());
+    
+    
+            // return $this->sendResponse( new SubjectResource ($subject), "Tantárgy frissítve" );
+
+            if(Subject::where('id', $id)->exists()){
+                $subject = Subject::find($id);
+                $subject->subject = $request->subject;
+    
+                $subject->save();
+    
+                return response()->json([
+                    'message' => "Sikeres adatrögzítés"
+                ], 200);
+    
+            }else{
+                return response()->json([
+                    'message' => "Sikertelen adatrögzítés"
+                ], 404);
+            
             }
-    
-            $subject = Subject::find( $id );
-            $subject->update( $request->all());
-    
-    
-            return $this->sendResponse( new SubjectResource ($subject), "Post frissítve" );
+
+
         }
     
         public function destroy($id){
     
-            Subject::destroy($id);
-            return $this->sendResponse([], "Post törölve");
+            // Subject::destroy($id);
+            // return $this->sendResponse([], "Tantárgy törölve");
+
+            if(Subject::where('id', $id)->exists()){
+                $subject = Subject::find($id);
+                $subject->delete();
+    
+                // $subject->save();
+    
+                return response()->json([
+                    'message' => "Sikeres törlés"
+                ], 200);
+    
+            }else{
+                return response()->json([
+                    'message' => "Sikertelen törlés"
+                ], 404);
+            
+            }
+        
         }
     }
